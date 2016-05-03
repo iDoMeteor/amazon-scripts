0dfdc2ea7ab15b8d7fca330be2299e8790f5de40
iDoMeteor <idometeor@gmail.com>
2016-05-03 15:02:24 -0400

    Testing

--- nginx-add-meteor-vhost.sh
+++ nginx-add-meteor-vhost.sh
@@ -43,7 +43,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # Check for arguments or provide help
-if [ ! -v "$1" ] ; then
+if [ $# -eq 0 ] ; then
   echo "Usage:"
   echo "  $0 -u user -h host [-v]"
   echo "  $0 --user user --host host [--verbose]"
