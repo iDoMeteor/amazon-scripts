b87c77d0bfd389888265c5257149d3c80a90bb65
iDoMeteor <idometeor@gmail.com>
2016-05-03 18:24:20 -0400

    Testing

--- meteor-bundle-and-deploy.sh
+++ meteor-bundle-and-deploy.sh
@@ -66,7 +66,7 @@ fi
 
 # Warn ec2-user or root
 ME=`whoami`
-if [[ ME =~ ^(ec2-user|root)$ ] ; then
+if [[ $ME =~ ^(ec2-user|root)$ ] ; then
   echo "You probably want to run this as an app user, rather than $ME\."
   read -p "Would you still like to proceed? [y/N]" -n 1 -r REPLY
   echo ""
