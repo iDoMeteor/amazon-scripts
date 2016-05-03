795bf0b4d6bc86e3fbdde5f9a20f5a53c46d7921
iDoMeteor <idometeor@gmail.com>
2016-05-03 14:29:58 -0400

    Testing

--- nginx-add-meteor-vhost.sh
+++ nginx-add-meteor-vhost.sh
@@ -85,7 +85,7 @@ if [ ! -v USERNAME ] ; then
   echo 'User name is required.'
   exit 1
 fi
-if (`getent passwd $USERNAME`) ; then
+if [ 0 -ne `getent passwd $USERNAME` ] ; then
   echo 'PAM user already exists.'
   exit 1
 fi
