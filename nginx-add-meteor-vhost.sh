c23107b13c844187dcf72b4bad781c3400fbf9ad
iDoMeteor <idometeor@gmail.com>
2016-05-03 12:24:42 -0400

    Testing

--- nginx-add-meteor-vhost.sh
+++ nginx-add-meteor-vhost.sh
@@ -81,17 +81,17 @@ do
 done
 
 # Validate required arguments
-if [ ! -n "$USERNAME" ] ; then
+if [ ! -v "$USERNAME" ] ; then
   echo 'User name is required.'
   exit 1
 fi
-if [ ! -n "$HOST" ] ; then
+if [ ! -v "$HOST" ] ; then
   echo 'Host name is required.'
   exit 1
 fi
 
 # Check verbosity
-if [ -n "$VERBOSE" ] ; then
+if [ -v "$VERBOSE" ] ; then
   set -v
 fi
 
