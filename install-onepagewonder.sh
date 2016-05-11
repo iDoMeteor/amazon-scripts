3be04becd0996545d1f8e33fe9723574bd202acd
iDoMeteor <idometeor@gmail.com>
2016-05-11 00:49:27 -0400

    Testing

--- install-onepagewonder.sh
+++ install-onepagewonder.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+ORIGIN=`pwd`
 /usr/local/bin/meteor-git-and-deploy.sh -r https://github.com/idometeor/onepagewonder -d onepagewonder
+cd $ORIGIN
--- install-rocketchat.sh
+++ install-rocketchat.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+ORIGIN=`pwd`
 /usr/local/bin/meteor-git-and-deploy.sh -r https://github.com/RocketChat/Rocket.Chat -d rocketchat
+cd $ORIGIN
